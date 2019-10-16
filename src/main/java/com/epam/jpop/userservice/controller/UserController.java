e639119f9d1482b53a00f41b0aeaa9549e0a97bc
rakeshvardan <rakesh_budugu@epam.com>
2019-10-16 06:03:42 +0530

    user-service: minor uri change

--- src/main/java/com/epam/jpop/userservice/controller/UserController.java
+++ src/main/java/com/epam/jpop/userservice/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/users")
+@RequestMapping("/api/users")
 @Api(value = "user-service")
 public class UserController {
 
